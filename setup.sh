ad556702a6835a0dbbc44c2069661e45ef5d1909
vincent <hillwater.zju@gmail.com>
2014-02-24 00:28:00 +0900

    add doc to the plugins

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 CUR_PATH=`pwd`
-ln -fs $CUR_PATH/vimrc ~/.vimrc
-ln -fs $CUR_PATH/plugin ~/.vim/plugin
-
 
 sudo apt-get install ctags
 sudo apt-get install cscope
+
+ln -fs $CUR_PATH/vimrc ~/.vimrc
+ln -fs $CUR_PATH/plugin ~/.vim/plugin
+ln -fs $CUR_PATH/doc ~/.vim/doc
+
+vim -c "helptags ~/.vim/doc" -c "q"
